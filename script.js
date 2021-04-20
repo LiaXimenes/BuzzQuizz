aeaeb5bd29b49dadbdf18be1ee3781ea604cca66
Laura Tissi <laura.tracierra@gmail.com>
2021-04-20 07:49:45 -0300

    ao clicar em add quizz ou fazer um quizz some os elementos de denro e abre a página

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+
+
+function sumirTelaPrincipal() {
+    let adicionarQuizzEscondido = document.querySelector(".criarQuizz");
+    adicionarQuizzEscondido.classList.add("escondido");
+
+    let quizzEscondido = document.querySelector(".todosQuizzes");
+    quizzEscondido.classList.add("escondido");
+}
+
 function criarQuizz() {
+    sumirTelaPrincipal()
+
     console.log("criar quizz, aguarde");
+
+    //adicionar tela pra criar quizz
 }
 
 function irParaQuizz(elemento){
+    sumirTelaPrincipal();
+
+    let telaDoQuizz = document.querySelector(".quizzEscolhido");
+    telaDoQuizz.classList.remove("escondido")
+
     console.log(elemento);
     alert("Me Clicou!");
 }
